9c9a7f0859f390c287f95232dd3c16045e43aa14
nathansozzi <102824810+nathansozzi@users.noreply.github.com>
2025-07-29 15:13:45 +0000

    Primeira versão do programa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -100,7 +100,7 @@ int main () {
     printf("Nome da Cidade: %s\n", nomeCidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f\n", pib1);
+    printf("PIB: R$ %.2f bilhões\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", numeroPontosTuristicos1);
 
     // CARTA 2
@@ -110,8 +110,11 @@ int main () {
     printf("Nome da Cidade: %s\n", nomeCidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f\n", pib2);
+    printf("PIB: R$ %.2f bilhões\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", numeroPontosTuristicos2);
+
+    //Espaço para formatação
+    printf("\n\n");
 
     return 0;
 
